4dbb1cedb6240a479c7be3df8991f68e92b079df
mary-clayton <mary.cathlean@gmail.com>
2020-09-10 19:16:37 -0700

    mvp done

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+import sys
 
 # Declare all the rooms
 
@@ -49,3 +51,25 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+player = Player("player1", room['outside'])
+
+for room in range(1):
+    print(f'Current room: {player.current_room.name}')
+    print(f'Description: {player.current_room.description}')
+
+move = input("Enter a move: ")
+
+if len(move) == 1:
+    if move != "s" and move != "n" and move != "w" and move != "e" and move != "q":
+        print("Invalid Move!")
+        print("Enter 's' to move south")
+        print("Enter 'n' to move north")
+        print("Enter 'w' to move west")
+        print("Enter 'e' to move east")
+        print("Enter 'q' to quit the game")
+
+    elif move == "q":
+        print("The player quit the game!")
+        sys.exit()
+
+    
--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room: 
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.s_to = ""
+        self.n_to = ""
+        self.w_to = ""
+        self.e_to = ""
+        self.item_list = []
+# Class is named Room and it contains a function called __init__
+# each attritbute is defined by their parameters
+# direction attributes are empty strings because it contains each item the room has
+# item_list is an empty array because it contains a set of items the room has
